dab4d12f0ab3fde4a1bccc40d1bfd065028bbb66
Vladimir <vladimir@vladimir.(none)>
2013-07-13 12:22:38 +0200

    Initial project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to personal-organiser
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/personal_organiser/core_test.clj
@@ -0,0 +1,7 @@
+(ns personal-organiser.core-test
+  (:require [clojure.test :refer :all]
+            [personal-organiser.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
