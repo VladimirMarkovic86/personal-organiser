520d7c7e6b96716212e365ed02cef68be247cf89
Vladimir <vladimir@vladimir.(none)>
2013-10-23 08:43:35 +0200

    Merge selenium branch with master;

--- src/clj/personal_organiser/repl.clj
+++ src/clj/personal_organiser/repl.clj
@@ -4,6 +4,9 @@
   provide convenience functions like 'go' and 'dev-server'."
   (:use [clojure.repl]
 	[clojure.java.shell :only [sh]])
+  (:import [java.io InputStreamReader
+		    BufferedReader]
+	   [java.lang Runtime])
   (:require [clojure.java.browse :as browse]
 	    [personal-organiser.server :as server]
 	    [personal-organiser.neo4j :as n4j]
@@ -16,32 +19,109 @@
 
 (defonce server (ref nil))
 
+(defn cmd-term
+  "Execute command prompt/terminal command"
+  [command]
+  (let [process (. (Runtime/getRuntime) exec command)
+      stdin (.getInputStream process)
+      isr (InputStreamReader. stdin)
+      br (BufferedReader. isr)]
+      (let [seq (line-seq br)]
+	seq)))
+
+(defn neo4j-linux
+  ""
+  []
+  (try (= (re-find #"Linux" (str (cmd-term "uname -a"))) "Linux")
+  (catch Exception e (do false))))
+
+(defn neo4j-win
+  ""
+  []
+  (try (= (re-find #"OSArchitecture" (str (cmd-term "wmic OS get OSArchitecture"))) "OSArchitecture")
+  (catch Exception e (do false))))
+
+(defn neo4j-stop-linux
+  ""
+  []
+  (cmd-term "neo4j-community/bin/neo4j stop"))
+
+(defn neo4j-stop-win
+  ""
+  []
+  (cmd-term "neo4j-community/bin/Neo4j.bat stop"))
+
+(defn neo4j-status-linux
+  ""
+  []
+  (neo4j-stop-linux)
+  false)
+
+(defn neo4j-status-win
+  ""
+  []
+  (neo4j-stop-win)
+  false)
+
+(defn neo4j-status
+  ""
+  []
+  (if (neo4j-linux)
+      (neo4j-status-linux)
+      (if (neo4j-win)
+	  (neo4j-status-win)
+	  false)))
+
+(defn neo4j-start-linux
+  ""
+  []
+  (cmd-term "neo4j-community/bin/neo4j start"))
+
+(defn neo4j-start-win
+  ""
+  []
+  (cmd-term "neo4j-community/bin/Neo4j.bat start"))
+
+(defn neo4j-start
+  "Start neo4j server"
+  []
+  (if (not (neo4j-status))
+	(if (neo4j-linux)
+	    (neo4j-start-linux)
+	    (if (neo4j-win)
+		(neo4j-start-win)))))
+
+(defn neo4j-stop
+  "Stop neo4j server"
+  []
+  (if (not (neo4j-status))
+	(if (neo4j-linux)
+	    (neo4j-stop-linux)
+	    (if (neo4j-win)
+		(neo4j-stop-win)))))
+
 (defn start-server
   "Start the development server and open the host application in the
   default browser."
   []
-  (if (= (str (:out (sh "neo4j-community/bin/neo4j" "status"))) "Neo4j Server is not running\n")
-      (println (:out (sh "neo4j-community/bin/neo4j" "start")))
-  )
+  (neo4j-start)
+  (Thread/sleep 15000)
   (n4j/connect-neo4j)
   (dosync (ref-set server (server/run-server)))
   (future (Thread/sleep 3000)
           (browse/browse-url "http://localhost:5000/login")))
 
-(defn restart-server
-  "Restart server"
-  []
-  (.stop @server)
-  (start-server))
-
 (defn stop-server
   "Stop server"
   []
-  (.stop @server)
-  (if (= (re-find #"Neo4j Server is running at pid " (:out (sh "neo4j-community/bin/neo4j" "status"))) "Neo4j Server is running at pid ")
-      (println (:out (sh "neo4j-community/bin/neo4j" "stop")))
-  )
-)
+  (neo4j-stop)
+  (.stop @server))
+
+(defn restart-server
+  "Restart server"
+  []
+  (stop-server)
+  (start-server))
 
 (defn -main [& args]
   (start-server))
@@ -50,4 +130,6 @@
 (println "Type (start-server) to launch server.")
 (println "Type (restart-server) to restart server.")
 (println "Type (stop-server) to stop server.")
+(println)
+(println "Type (selenium-test) to test functionalities.")
 (println)
--- src/clj/personal_organiser/selenium/selenium_test.clj
+++ src/clj/personal_organiser/selenium/selenium_test.clj
@@ -1,38 +1,58 @@
 (ns personal-organiser.selenium.selenium-test
-  (:import [org.openqa.selenium.server SeleniumServer]
-	   [com.thoughtworks.selenium DefaultSelenium
-				      Selenium]))
+  (:require [clj-webdriver.taxi :as tx]))
 
-(defn start-selenium-server
-  "Start selenium server and open browser"
+(defn start-selenium-session
+  "Start selenium session and open browser"
   []
-  (def url "/login")
-  (def selenium-server (SeleniumServer.))
-  (def selenium (DefaultSelenium. "localhost" 4444 "*firefox" "http://localhost:5000/"))
-  (.start selenium-server)
-  (.start selenium)
+  (tx/set-driver! {:browser :firefox} "http://localhost:5000/login")
 )
 
-(defn stop-selenium-server
-  "Stop selenium server and close browser"
+(defn stop-selenium-session
+  "Stop selenium session and close browser"
   []
-  (.stop selenium)
-  (.stop selenium-server))
+  (tx/quit))
 
 (defn login
   "Login into application"
   []
-  (.open selenium url)
-  (.type selenium "email" "bla@bla.com")
-  (.type selenium "password" "bla.1")
-  (.click selenium "//input[@value='Login']")
-  (.waitForPageToLoad selenium "30000")
-  (= (.getText selenium "//div[@class='made-by']/div[2]") "email: markovic.vladimir86@gmail.com")
+  (tx/input-text "#email" "email@mail.com")
+  (tx/input-text "#password" "email.1")
+  (tx/submit "#password")
+)
+
+(defn delete-organism
+  "Delete organism from application"
+  []
+  (tx/click "a[href='/organism-nav']")
+  (tx/click "a[href*='/read-organism/']")
+  (tx/click "a[href*='/delete-organism/']")
+)
+
+(defn register
+  "Register into application"
+  []
+  (tx/click "a[id='register']")
+  (tx/input-text "#ofirst-name" "Firstname")
+  (tx/input-text "#olast-name" "Lastname")
+  (tx/input-text "#oemail" "email@mail.com")
+  (tx/input-text "#opassword" "email.1")
+  (tx/input-text "#oconfirm-password" "email.1")
+  (tx/input-text "#oheight" "179")
+  (tx/input-text "#oweight" "69")
+  (tx/select-by-value "select[id='obirthday-year']" "1980")
+  (tx/select-by-value "select[id='obirthday-month']" "03")
+  (tx/select-by-value "select[id='obirthday-day']" "25")
+  (tx/click "#ogender-male")
+  (tx/click "#odiet-all")
+  (tx/click "#oactivity-easy")
+  (tx/submit "#opassword")
 )
 
 (defn test-suite
   "Execute test suite"
   []
-  (start-selenium-server)
+  (start-selenium-session)
+  (register)
   (login)
-  (stop-selenium-server))
+  (delete-organism)
+  (stop-selenium-session))
