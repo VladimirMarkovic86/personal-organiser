14a3ca70dde069eed666056299c6f55646978ae6
Vladimir <vladimir@vladimir.(none)>
2013-11-06 19:22:56 +0100

    Dependency changed for drools library

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
 		 [compojure "1.1.5"]
 		 [com.cemerick/valip "0.3.2"]
-		 [clojurewerkz/neocons "1.1.0"]
+		 [clojurewerkz/neocons "2.0.0-beta3"]
 		 [ring/ring-core "1.2.0"]
 		 [ring/ring-jetty-adapter "1.1.0"]
 		 [enlive "1.1.1"]
@@ -25,7 +25,7 @@
 		 [xml-apis/xml-apis "1.4.01"]
 		 [clj-webdriver "0.6.0"]
 ;		 Drools runtime dependencies should bi commented when compiling clojurescript
-		 [org.antlr/antlr-runtime "3.5"] [org.drools/drools-core "6.0.0.CR1"] [org.drools/drools-compiler "6.0.0.CR1"] [org.drools/drools-decisiontables "6.0.0.CR1"] [org.drools/droolsjbpm-ide-common "5.5.0.Final"] [org.drools/drools-jsr94 "6.0.0.CR1"] [org.drools/drools-templates "6.0.0.CR1"] [org.drools/guvnor-api "5.5.0.Final"] [org.jbpm/jbpm-bpmn2 "6.0.0.CR1"] [org.jbpm/jbpm-flow "6.0.0.CR1"] [org.jbpm/jbpm-flow-builder "6.0.0.CR1"] [drools/drools-jsr94 "20040726.195413"] [junit/junit "4.11"] [jexcelapi/jxl "2.6"] [org.drools/knowledge-api "6.0.0.CR1"] [org.drools/knowledge-internal-api "5.5.0.Final"] [org.mvel/mvel2 "2.1.6.Final"] [org.eclipse.jdt/core "3.3.0-v_771"] [org.slf4j/slf4j-api "1.7.5"] [xmlpull/xmlpull "1.1.3.4d_b4_min"] [xpp3/xpp3_min "1.1.4c"] [xstream/xstream "1.2.2"] [org.jvnet.jaxb2_commons/jaxb-xjc "2.1.10.1"]
+		 [org.antlr/antlr-runtime "3.5"] [org.drools/drools-core "6.0.0.CR1"] [org.drools/drools-compiler "6.0.0.CR1"] [org.drools/drools-decisiontables "6.0.0.CR1"] [org.drools/droolsjbpm-ide-common "5.5.0.Final"] [org.drools/drools-jsr94 "6.0.0.CR1"] [org.drools/drools-templates "6.0.0.CR1"] [org.drools/guvnor-api "5.5.0.Final"] [org.jbpm/jbpm-bpmn2 "6.0.0.CR1"] [org.jbpm/jbpm-flow "6.0.0.CR1"] [org.jbpm/jbpm-flow-builder "6.0.0.CR1"] [drools/drools-jsr94 "20040726.195413"] [junit/junit "4.11"] [jexcelapi/jxl "2.6"] [org.drools/knowledge-api "6.0.0.CR1"] [org.drools/knowledge-internal-api "5.5.0.Final"] [org.mvel/mvel2 "2.1.6.Final"] [org.eclipse.jdt/org.eclipse.jdt.core "3.7.1"] [org.slf4j/slf4j-api "1.7.5"] [xmlpull/xmlpull "1.1.3.4d_b4_min"] [xpp3/xpp3_min "1.1.4c"] [xstream/xstream "1.2.2"] [org.jvnet.jaxb2_commons/jaxb-xjc "2.1.10.1"]
 ]
 
   ;; run main from personal-organiser.core namespace
